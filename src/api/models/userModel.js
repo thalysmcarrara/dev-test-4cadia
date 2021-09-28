5138b014cb6ec2cd0bd6dfa9c17a8b62b8313777
thalysmcarrara <thalysmatiascarrara@gmail.com>
2021-09-28 11:48:31 -0400

    removing business rule from the model layer

--- src/api/models/userModel.js
+++ src/api/models/userModel.js
@@ -12,31 +12,16 @@ const findByEmail = async (email) => {
   return result;
 };
 
-const signup = async ({ name, email, password }) => {
-  const exists = await findByEmail(email);
-
-  if (exists) return false;
-
-  const releaseDate = new Date();
-
-  const userWithDate = {
-    name,
-    email,
-    password,
-    releaseDate,
-  };
-
+const signup = async (user) => {
   const db = await mongoConnection.getConnection();
   
-  const result = await db.collection(USERS).insertOne(userWithDate); 
+  const result = await db.collection(USERS).insertOne(user); 
 
   if (!result.acknowledged) return false;
 
   return { user: {
-    name,
-    email,
-    releaseDate,
     _id: result.insertedId.toString(),
+    ...user,
   } };
 };
 

83ca9c4f7e4baf2e88cc5b3780b6c6e691379867
thalysmcarrara <thalysmatiascarrara@gmail.com>
2021-09-28 20:46:38 -0400

    userService layer tests refactored

--- src/_tests/services/userService.test.js
+++ src/_tests/services/userService.test.js
@@ -1,7 +1,25 @@
+const sinon = require('sinon');
 const { expect } = require('chai');
 
-const userService = {
-  signup: () => {}
+const userService = require('../../api/services/userService');
+const userModel = require('../../api/models/userModel');
+
+const userMock = {
+  _id: '61534953a59a3b9bf7719e82',
+  name: 'John doe',
+  email: 'johndoe@gmail.com',
+  password: '$2b$10$wkehE5MXWgdRNMZfs6C.sOZWOoMqzK.Spbs5mUis5Tc4bNfBB3hh2',
+  releaseDate: '2021-09-28T16:56:51.456Z'
+}
+
+const signupMock = {
+  user: {
+    _id: '61534953a59a3b9bf7719e82',
+  name: 'John doe',
+  email: 'johndoe@gmail.com',
+  password: '$2b$10$wkehE5MXWgdRNMZfs6C.sOZWOoMqzK.Spbs5mUis5Tc4bNfBB3hh2',
+  releaseDate: '2021-09-28T16:56:51.456Z'
+  }
 }
 
 const user = {
@@ -12,6 +30,13 @@ const user = {
 
 describe('testing the service layer of the /register route' , () => {
   describe('when the user already exists', () => {
+    before(() => {
+      sinon.stub(userModel, 'findByEmail').resolves(userMock)
+    });
+
+    after(async () => {
+      await userModel.findByEmail.restore();
+    })
     it('returns an object with an error property', async () => {
       const result = await userService.signup(user);
       expect(result).to.be.an('object');
@@ -33,6 +58,15 @@ describe('testing the service layer of the /register route' , () => {
   });
 
   describe('when the user does not exist', () => {
+    before(() => {
+      sinon.stub(userModel, 'findByEmail').resolves(null);
+      sinon.stub(userModel, 'signup').resolves(signupMock);
+    });
+
+    after(async () => {
+      await userModel.findByEmail.restore();
+      await userModel.signup.restore();
+    });
     it('returns an object with the user property', async () => {
       const result = await userService.signup(user);
       expect(result).to.be.a('object');

9d43b5f0e8d56a64abf53136813bb26da491f77c
thalysmcarrara <thalysmatiascarrara@gmail.com>
2021-09-28 22:16:17 -0400

    userController layer finished

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -1,5 +1,15 @@
-const signup = async (req, res) => {
+const userService = require('../services/userService');
 
+const signup = async (req, res) => {
+  const user = req.body;
+
+  const result = await userService.signup(user);
+
+  const { error } = result;
+
+  if (error) return res.status(error.code).json({ message: error.message });
+
+  return res.status(201).json(result);
 };
 
 module.exports = { signup };
